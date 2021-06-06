0e78d76f0f31bb2c20560f3c353f62ed4a04d6a8
tjjap <tiongjangjap@gmail.com>
2021-06-06 18:26:15 +0700

    create Pipeline model

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Pipeline(models.Model):
+    directorate = models.CharField(max_length=10, choices=[('Public-1', 'Public-1'), ('Public-2', 'Public-2'), ('Public-3', 'Public-3'), ('Private-1', 'Private-1'), ('Private-2', 'Private-2')])
+    ambm = models.CharField(max_length=30)
+    pid = models.CharField(max_length=10, unique=True)
+    client = models.CharField(max_length=50)
+    project = models.CharField(max_length=50)
+    rental = models.BooleanField(default=False)
+    nmonth = models.IntegerField
+    level = models.CharField(max_length=2)
+    order_dd = models.DateField
+    order_val = models.DecimalField(max_digits=17, decimal_places=2)
+    gpm = models.DecimalField(max_digits=6, decimal_places=2)
+    bast_dd = models.DateField
