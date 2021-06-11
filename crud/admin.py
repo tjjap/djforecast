2ee1a56a7de153ae784d13f62a4914dd0c6ea879
tjjap <tjjap@yahoo.com>
2021-06-11 11:15:56 +0700

    format decimal right align

--- crud/admin.py
+++ crud/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
+from django import forms
 from .models import Pipeline
 from django.conf.locale.es import formats as es_formats
+from django.db import models
+from django.forms.widgets import TextInput, Textarea
 
 
-# Customize Admin Panel (Edit Panel) 
+# Customize Admin Panel (Edit Panel)
+
 class PipelineAdmin(admin.ModelAdmin):
     list_filter = ("directorate", "ambm")
     list_display = ("directorate", "ambm", "pid", "client", "project", "level", "format_order_val", "format_gpm", "format_order_dd", "rental")
+    #list_display = ("format_order_val", "directorate", "ambm", "pid", "client", "project", "level", "format_order_val")
+    date_hierarchy = 'order_dd'
+    
+
+    # formfield_overrides = {
+    #     models.DecimalField: {
+    #         'widget': forms.Textarea(attrs={'style': 'text-align:right;',}),
+    #     }
+    # }
+
 
 # Register your models here.
 admin.site.register(Pipeline, PipelineAdmin)
--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 
 # Create your models here.
@@ -7,13 +8,14 @@ class Pipeline(models.Model):
     pid = models.CharField('Prospect ID', max_length=10, unique=True)
     client = models.CharField('Client Name', max_length=50)
     project = models.CharField('Project Name', max_length=50)
-    rental = models.BooleanField('Rental/Mngd Service', default=False)
+    rental = models.BooleanField('Rental', default=False)
     nmonth = models.IntegerField('x Months', default=1)
-    level = models.CharField('Level', max_length=2)
+    level = models.CharField('Level', max_length=2, choices=[('A', 'A'), ('B', 'B'), ('C', 'C'), ('D+', 'D+'), ('D', 'D'), ('E', 'E')])
     order_dd = models.DateField('SO Booking DD')
     order_val = models.DecimalField('Value', max_digits=14, decimal_places=0)
     gpm = models.DecimalField('GPM (%)', max_digits=6, decimal_places=2)
     bast_dd = models.DateField('BAST Date')
+
 
     # Reformat order_dd
     def format_order_dd(self):
@@ -23,16 +25,20 @@ class Pipeline(models.Model):
     format_order_dd.short_description = 'SO Date'
 
     # Reformat order_val to in Million with thousand separator
+    @admin.display(description='In Million Rp') # Column Header
     def format_order_val(self):
+        from django.utils.html import format_html
         order_mil = self.order_val / 1000000
-        return '{:,}'.format(order_mil)
-    
-    # Column Header
-    format_order_val.short_description = 'VALUE IN MILL'
+        formatted_number = format(order_mil, ',.0f')
+        return format_html("<p style='text-align:right;'>{}</p>", formatted_number)
 
-    # Reformat gpm to add %
+    # Reformat gpm to align right and add %
+    @admin.display(description='== GPM ==') # Column Header
     def format_gpm(self):
-        return '{:.2f}%'.format(self.gpm)
-    
-    # Column Header
-    format_gpm.short_description = 'GPM'
+        from django.utils.html import format_html
+        formatted_gpm = format(self.gpm, ',.2f')
+        return format_html("<p style='text-align:right;'>{}%</p>", formatted_gpm)
+
+    class Meta:
+        verbose_name_plural = "Sales Pipelines"
+        #ordering = ['-pid']
