cb64af23d244960e72b0528ba3291629bc88ceb1
tjjap <tiongjangjap@gmail.com>
2021-06-13 08:09:33 +0700

    custom admin page in change list and update

--- crud/admin.py
+++ crud/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django import forms
-from .models import Pipeline
+from django.contrib.admin.options import ModelAdmin
+from .models import Pipeline, Level
 from django.conf.locale.es import formats as es_formats
 from django.db import models
 from django.forms.widgets import TextInput, Textarea
@@ -10,7 +11,7 @@ from django.forms.widgets import TextInput, Textarea
 
 class PipelineAdmin(admin.ModelAdmin):
     list_filter = ("directorate", "ambm")
-    list_display = ("directorate", "ambm", "pid", "client", "project", "level", "format_order_val", "format_gpm", "format_order_dd", "rental")
+    list_display = ("directorate", "ambm", "pid", "client", "project", "level", "format_order_val", "format_gpm", "format_order_dd", "rental",)
     #list_display = ("format_order_val", "directorate", "ambm", "pid", "client", "project", "level", "format_order_val")
     date_hierarchy = 'order_dd'
     
@@ -21,8 +22,12 @@ class PipelineAdmin(admin.ModelAdmin):
     #     }
     # }
 
+class LevelAdmin(admin.ModelAdmin):
+    list_display = ("level", "format_weight",)
+
 
 # Register your models here.
 admin.site.register(Pipeline, PipelineAdmin)
+admin.site.register(Level, LevelAdmin)
 
 
