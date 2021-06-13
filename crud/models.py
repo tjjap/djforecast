c424bd4e80fec37eeeb21f5b89c8dcafd34afd4e
tjjap <tiongjangjap@gmail.com>
2021-06-13 08:07:51 +0700

    create chande list and update llist page

--- crud/models.py
+++ crud/models.py
@@ -10,7 +10,7 @@ class Pipeline(models.Model):
     project = models.CharField('Project Name', max_length=50)
     rental = models.BooleanField('Rental', default=False)
     nmonth = models.IntegerField('x Months', default=1)
-    level = models.CharField('Level', max_length=2, choices=[('A', 'A'), ('B', 'B'), ('C', 'C'), ('D+', 'D+'), ('D', 'D'), ('E', 'E')])
+    level = models.ForeignKey('Level', on_delete=models.CASCADE)
     order_dd = models.DateField('SO Booking DD')
     order_val = models.DecimalField('Value', max_digits=14, decimal_places=0)
     gpm = models.DecimalField('GPM (%)', max_digits=6, decimal_places=2)
@@ -27,18 +27,37 @@ class Pipeline(models.Model):
     # Reformat order_val to in Million with thousand separator
     @admin.display(description='In Million Rp') # Column Header
     def format_order_val(self):
-        from django.utils.html import format_html
         order_mil = self.order_val / 1000000
-        formatted_number = format(order_mil, ',.0f')
-        return format_html("<p style='text-align:right;'>{}</p>", formatted_number)
+        formatted_number = str(format(order_mil, ',.0f'))
+        padding = 9 - len(formatted_number)
+        new_format = '_' * padding + formatted_number
+        return new_format
+
 
     # Reformat gpm to align right and add %
     @admin.display(description='== GPM ==') # Column Header
     def format_gpm(self):
-        from django.utils.html import format_html
-        formatted_gpm = format(self.gpm, ',.2f')
-        return format_html("<p style='text-align:right;'>{}%</p>", formatted_gpm)
+        formatted_gpm = format(self.gpm, '.2f')
+        return f"{formatted_gpm}%"
 
     class Meta:
         verbose_name_plural = "Sales Pipelines"
         ordering = ['order_dd', 'level', '-order_val']
+
+
+class Level(models.Model):
+    level = models.CharField('Level', max_length=2)
+    weight = models.DecimalField('Weight', max_digits=6, decimal_places=2) #in %
+
+    def __str__(self):
+        return self.level
+    
+    @admin.display(description='Weight %') # Column Header
+    def format_weight(self):
+        from django.utils.html import format_html
+        formatted_weight = format(self.weight, ',.2f')
+        return f"{formatted_weight}%"
+
+    class Meta:
+        verbose_name_plural = "Levels"
+        ordering = ['level']
